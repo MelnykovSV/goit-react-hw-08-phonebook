0a8efd8ed09d3dd8bcf30275fd1f8a244df50187
MelnykovSV <svmelnykov@gmail.com>
2023-06-02 02:54:21 +0300

    1

--- src/components/SharedLayout/ResponsiveAppBar.tsx
+++ src/components/SharedLayout/ResponsiveAppBar.tsx
@@ -15,6 +15,7 @@ import AdbIcon from '@mui/icons-material/Adb';
 import { NavLink, Outlet } from 'react-router-dom';
 import { useAppSelector } from '../../redux/hooks';
 import { getToken } from '../../redux/auth/authSlice';
+import { UserMenu } from '../UserMenu/UserMenu';
 
 // const pages = ['register', 'login', 'contacts'];
 const publicPages = ['register', 'login'];
@@ -123,36 +124,40 @@ export function ResponsiveAppBar() {
               ) : null
             )}
           </Box>
+          {token ? (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Demy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map(setting => (
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <UserMenu />
+
+                {/* {settings.map(setting => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              ))} */}
+              </Menu>
+            </Box>
+          ) : null}
         </Toolbar>
       </Container>
     </AppBar>
