7d6c1af1716dc886884bfa4b9e3c086d40d8e9c9
MelnykovSV <svmelnykov@gmail.com>
2023-06-02 22:41:13 +0300

    1

--- src/UserMenu/PrivateRoute.tsx
+++ src/UserMenu/PrivateRoute.tsx
@@ -2,13 +2,11 @@ import { Navigate } from 'react-router-dom';
 
 import { useAppSelector } from '../redux/hooks';
 
-// import { getIsLoggedIn } from '../redux/auth/authSlice';
 import { getToken } from '../redux/auth/authSlice';
 import { IRouteProps } from '../interfaces';
 
 export default function PrivateRoute({ children, ...routeProps }: IRouteProps) {
   const token = useAppSelector(getToken);
-  console.log(token);
   return (
     <div {...routeProps}>{token ? children : <Navigate to="/login" />}</div>
   );
